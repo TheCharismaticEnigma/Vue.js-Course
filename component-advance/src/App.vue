007381d645ccefa095b259db5c68e62d57006cb6
TheCharismaticEnigma <avishkar2002@mail.com>
2023-02-21 22:42:27 +0530

    Section CheckPoint

--- component-advance/src/App.vue
+++ component-advance/src/App.vue
@@ -11,7 +11,17 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    TheHeader, // TheHeader: TheHeader
+    BadgeList,
+    UserInfo: UserInfo,
+  },
+
   data() {
     return {
       activeUser: {
@@ -24,7 +34,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 html {
   font-family: sans-serif;
 }
---
+++ component-advance/src/components/BaseCard.vue
@@ -0,0 +1,46 @@
+// Wrapps another component to make it a card.
+
+<template>
+  <div>
+    <header v-if="$slots.header">
+      <slot name="header"></slot>
+    </header>
+
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h2 {
+  margin: 2rem 0;
+  color: #3a3a3a;
+  text-align: center;
+}
+
+ul.badge-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin-right: 1rem;
+}
+</style>
